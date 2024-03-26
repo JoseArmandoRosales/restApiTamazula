a2abe05f0d03f5870069c51aaca20d3a9986fe47
JoseArmandoRosales <jose_rosalesr@hotmail.com>
2024-03-26 12:01:47 -0700

    GET client changes

--- modelo/postgress/clientes.js
+++ modelo/postgress/clientes.js
@@ -10,6 +10,8 @@ const DB_CONFIG = {
     ssl: true
 }
 
+const getquery = 'SELECT c.nombre, c.apellidopaterno, c.apellidomaterno, co.colonia, c.calle, c.telefono, c.telefonoalt, r.fechaentrega, r.fecharecoger, e.estatus FROM public.clientes c JOIN rentas r ON c.id = r.clienteid JOIN colonias co ON c.coloniaid = co.id JOIN estatusrentas e ON r.estatusid = e.id;'
+
 
 const client = new pg.Client(DB_CONFIG)
 await client.connect()
@@ -17,7 +19,8 @@ await client.connect()
 
 export class ClienteModelo {
     static async getAll({ renta }) {
-        const query = await client.query('SELECT * FROM clientes ORDER BY id ASC LIMIT 100');
+        const query = await client.query(getquery);
+        console.log( query.rows[1] )
         return query.rows
     }
 
