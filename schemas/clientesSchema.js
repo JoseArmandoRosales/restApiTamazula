49a98cde3f506d9007c7af4ddf151347e1b23071
JoseArmandoRosales <jose_rosalesr@hotmail.com>
2024-04-15 17:15:20 -0700

    cliente zod schema improved

--- schemas/clientesSchema.js
+++ schemas/clientesSchema.js
@@ -1,21 +1,15 @@
 import z from 'zod'
 
 const clienteSchema = z.object({
-    nombre: z.string(),
-    apellidoPaterno: z.string(),
-    apellidoMaterno: z.string(),
-    curp: z.string(),
-    colonia: z.string(),
-    calle: z.string(),
-    numeroTelefono: z.string().max(10).min(7),
-    renta: z.string()
+    nombre: z.string().max(50).nullable(),
+    apellidopaterno: z.string().max(30).nullable(),
+    apellidomaterno: z.string().max(30).nullable(),
+    coloniaid: z.number().nullable(),
+    calle: z.string().max(50).nullable(),
+    telefono: z.string().max(16).min(7).nullable(),
+    telefonoalt: z.string().max(16).min(7).nullable()
 })
 
 export function validateCliente (input){
-    return clienteSchema.safeParse(input)
-}
-
-export function validatePartialCliente (input){
     return clienteSchema.partial().safeParse(input)
 }
-
